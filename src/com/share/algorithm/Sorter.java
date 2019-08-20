1296af6583491966af2ff8f29b610063e47221c1
manson <v-mpeng@expedia.com>
2019-08-20 14:19:19 +0800

    merge sort

--- src/com/share/algorithm/Sorter.java
+++ src/com/share/algorithm/Sorter.java
@@ -1,7 +1,7 @@
 package com.share.algorithm;
 
 public class Sorter<T extends Comparable<T>> {
-    public static <T extends Comparable<T>> T[] selectionSort(T[] arr) {
+    public static <T extends Comparable<T>> void selectionSort(T[] arr) {
         int len = arr.length;
         int index = 0;
         for (int i = 0; i < len - 1; i++) {
@@ -15,10 +15,9 @@ public class Sorter<T extends Comparable<T>> {
             swap(arr, i, index);
 
         }
-        return arr;
     }
 
-    public static <T extends Comparable<T>> T[] insertionSort(T[] arr) {
+    public static <T extends Comparable<T>> void insertionSort(T[] arr) {
         int len = arr.length;
         T temp = null;
         for (int i = 1; i < len; i++) {
@@ -29,12 +28,9 @@ public class Sorter<T extends Comparable<T>> {
             }
             arr[j + 1] = temp;
         }
-
-        return arr;
-
     }
 
-    public static <T extends Comparable<T>> T[] bubboSort(T[] arr) {
+    public static <T extends Comparable<T>> void bubboSort(T[] arr) {
         int len = arr.length;
         for (int i = 0; i < len; i++) {
             for (int j = 1; j < len - i; j++) {
@@ -43,8 +39,42 @@ public class Sorter<T extends Comparable<T>> {
                 }
             }
         }
+    }
 
-        return arr;
+    public static <T extends Comparable<T>> void mergeSort(T[] arr) {
+        mergeSort(arr, 0, arr.length);
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(T[] arr, int start, int end) {
+        if ((end - start) == 1) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid, end);
+        int rightIndex = mid, leftIndex = start;
+
+        T[] ret = (T[]) new Comparable[end - start];
+        for (int i = 0; i < end - start; i++) {
+            if (leftIndex >= mid) {
+                ret[i] = arr[rightIndex];
+                rightIndex++;
+            } else if (rightIndex >= end) {
+                ret[i] = arr[leftIndex];
+                leftIndex++;
+            } else if (arr[leftIndex].compareTo(arr[rightIndex]) < 0) {
+                ret[i] = arr[leftIndex];
+                leftIndex++;
+            } else {
+                ret[i] = arr[rightIndex];
+                rightIndex++;
+            }
+        }
+
+        for (int i = start; i < end; i++) {
+            arr[i] = ret[i - start];
+        }
     }
 
     public static <T extends Comparable<T>> void swap(T[] arr, int i, int j) {
@@ -54,10 +84,11 @@ public class Sorter<T extends Comparable<T>> {
     }
 
     public static void main(String[] args) {
-        Integer[] intArr = {1, 3, 4, 2, 68, 6, 7};
+        Integer[] intArr = {1, 3, 4, 2, 68, 6, 7,10};
         //selectionSort(intArr);
         //insertionSort(intArr);
-        bubboSort(intArr);
+        //bubboSort(intArr);
+        mergeSort(intArr);
         for (int i = 0; i < intArr.length; i++) {
             System.out.println(intArr[i]);
         }
